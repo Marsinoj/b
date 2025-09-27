0f4567a02160a44e934da52ca2c7bdef8fe87fb0
Mariel Inojales <mariel.inojales@hcdc.edu.ph>
2025-09-27 12:37:51 +0800

    initial commit

---
+++ valentine-confession.js
@@ -0,0 +1,30 @@
+$("#messageState").on("change", (x) => {
+	$(".message").removeClass("openNor").removeClass("closeNor");
+	if ($("#messageState").is(":checked")) {
+		$(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
+		$(".cake").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
+		$(".container").stop().animate({"backgroundColor": "#f48fb1"}, 2000);
+		console.log("Abrindo");
+	} else {
+		$(".message").removeClass("no-anim").addClass("closeNor");
+		$(".cake").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
+		$(".container").stop().animate({"backgroundColor": "#fce4ec"}, 2000);
+		console.log("fechando");
+	}
+});
+
+$(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+	console.log("Animation End");
+	if ($(".message").hasClass("closeNor"))
+		$(".message").addClass("closed");
+	$(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
+});
+
+$(".cake").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+	console.log("Animation End");
+	if (!$(".cake").hasClass("closeHer"))
+		$(".cake").addClass("openedHer").addClass("beating");
+	else
+		$(".cake").addClass("no-anim").removeClass("beating");
+	$(".cake").removeClass("openHer").removeClass("closeHer");
+});
